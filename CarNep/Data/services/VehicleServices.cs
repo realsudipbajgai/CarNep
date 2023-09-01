16b563b0362162e697fb3323cda1a72b202a890c
Sudip <83912230+realsudipbajgai@users.noreply.github.com>
2023-09-01 14:20:47 -0500

    Generic Repo and filtering is done

--- CarNep/Data/services/VehicleServices.cs
+++ CarNep/Data/services/VehicleServices.cs
@@ -3,32 +3,36 @@ using CarNep.Data.Helpers;
 using CarNep.Data.repo;
 using CarNep.Data.ViewModel;
 using DAL.Data;
+using DAL.Generic_Repositories;
+using DAL.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarNep.Data.services
 {
-    public class VehicleServices : IVehicleServices
+    public class VehicleServices :GenericRepository<Vehicle,int>,IVehicleServices
     {
-        private readonly AppDbContext _context;
         private readonly IMapper _mapper;
-        public VehicleServices(AppDbContext context, IMapper mapper )
+
+        public VehicleServices(IMapper mapper,AppDbContext context):base(context)
         {
-            _context=context;
             _mapper=mapper;
         }
-        public List<VehicleVM> GetAll()
+        public List<VehicleVM> GetAllVehicles()
         {
-            var vehicles = _context.Vehicles.ToList();
-            var vehicleListVM=_mapper.Map<List<VehicleVM>>(vehicles);
+            var vehicles = GetAll(
+                x=>x.Include(v=>v.Brand)
+                    .Include(v=>v.Category)
+            );
+            var vehicleListVM = _mapper.Map<List<VehicleVM>>(vehicles);
             return vehicleListVM;
         }
 
-        public VehicleVM GetById(int id)
+        public VehicleVM GetVehicleById(int id)
         {
-            var vehicle=_context.Vehicles
-                .Include(v=>v.Brand)
-                .Include(v=>v.Category)
-                .FirstOrDefault(v => v.Id == id);
+            var vehicle = GetById(x=>x.Id==id,
+                x=>x.Include(v=>v.Brand)
+                    .Include(v=>v.Category)
+                );
             var vehicleVM = _mapper.Map<VehicleVM>(vehicle);
             return vehicleVM;
         }
